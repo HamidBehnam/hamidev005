76af9ab506adaf38e9574c3975e24ddfb992bd7f
Hamid Behnam <hamid.behnam@gmail.com>
2019-03-21 16:20:57 -0700

    add auth and pages modules and their routing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'auth',
+  loadChildren: './auth/auth.module#AuthModule'
+}, {
+  path: '**',
+  loadChildren: './pages/pages.module#PagesModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
