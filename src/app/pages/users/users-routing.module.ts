59e2482d6ea084847176540b468cdac40b706312
Hamid Behnam <hamid.behnam@gmail.com>
2019-03-21 19:30:53 -0700

    add users list and users detail components

--- src/app/pages/users/users-routing.module.ts
+++ src/app/pages/users/users-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {UsersComponent} from './users.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import {UsersDetailComponent} from './users-detail/users-detail.component';
 
 const routes: Routes = [{
   path: '',
   component: UsersComponent,
-  children: []
+  children: [{
+    path: '',
+    component: UsersListComponent
+  }, {
+    path: ':id',
+    component: UsersDetailComponent
+  }]
 }];
 
 @NgModule({
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import {UsersRoutingModule} from './users-routing.module';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersDetailComponent } from './users-detail/users-detail.component';
 
 @NgModule({
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, UsersListComponent, UsersDetailComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
